d6485193552e081bd4ddc9d972ae2430d6fc46b5
caheredia <cristianheredia@gmail.com>
2018-10-25 20:01:56 -0700

    add new file and metrics

--- abt.py
+++ abt.py
@@ -8,7 +8,11 @@ lines = [line.replace(',', '').replace('\'', '').replace('.', '').lower().split(
          for line in file_lines]
 
 words = [word for line in lines for word in line]
-
+print('No more than 2.6%')
+print('\n')
 print('AND index: {:.2%}'.format(words.count('and') / len(words)))
 # NARRATIVE INDEX =  (BUT/AND)  X  100
+print('\n')
+print('Boring texts usually score under 10, average is in the teens, \nabove average is 20’s, powerful is 30’s, and 40 or over is incredibly rare')
+print('\n')
 print('Narrative index: {:.2%}'.format(words.count('but') / words.count('and')))
